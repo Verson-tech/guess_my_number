d8011d5663aa7b930619ea39fa1f7858197d6514
vira.murray <vira.avramenko@gmail.com>
2022-05-03 16:36:33 +0200

    .again button

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // console.log(document.querySelector('.guess').value);
 
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function(){
@@ -44,4 +44,14 @@ const guess = Number(document.querySelector('.guess').value);
         document.querySelector('.score').textContent = 0;
        }
    }
+})
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor= '#222';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').style.width = '15rem'
 })
